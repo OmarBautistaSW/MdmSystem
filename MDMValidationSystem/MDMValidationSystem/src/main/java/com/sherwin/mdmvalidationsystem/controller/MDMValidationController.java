6805ab4a1992d517e00643c8be4193ed318d6557
Omar E Bautista <Omar.Bautista@sherwin.com.mx>
2017-10-09 11:19:04 -0500

    Item WF inicio

--- MDMValidationSystem/MDMValidationSystem/src/main/java/com/sherwin/mdmvalidationsystem/controller/MDMValidationController.java
+++ MDMValidationSystem/MDMValidationSystem/src/main/java/com/sherwin/mdmvalidationsystem/controller/MDMValidationController.java
@@ -2,6 +2,7 @@ package com.sherwin.mdmvalidationsystem.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,5 +12,11 @@ public class MDMValidationController {
 	@RequestMapping(path={"/"},method=RequestMethod.GET)
 	public String indexMDM(Model model){
 		return "index";
+	}
+	
+	@RequestMapping(path={"/ItemWFValidation"},method=RequestMethod.POST)
+	public String itemWFValidation (@ModelAttribute("region") String region,Model model){
+		model.addAttribute("region",region.toUpperCase());
+		return "ItemWFValidation";
 	}
 }
